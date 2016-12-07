08eeab1a42ef758d8d12f1c53b698628622bcf15
Zack Scholl <zack.scholl@gmail.com>
2016-12-07 06:47:03 -0500

    Added pibakery, fixed initialize

--- node/initialize.sh
+++ node/initialize.sh
@@ -2,14 +2,13 @@
 
 # Update
 apt-get update
-apt-get install vim git python3 python3-rpi.gpio python3-requests
+apt-get install htop vim git python3 python3-rpi.gpio python3-requests
 
 # Install wireshark and other basics using all defaults
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install wireless-tools firmware-atheros usbutils wireshark tshark 
 
 # Clone the scanning repo
-cd /home/pi && git clone https://schollz@bitbucket.org/schollz/lfox-sonar.git
-cd /home/pi/lfox-sonar && git reset --hard HEAD && git pull
+cd /home/pi && git clone https://github.com/schollz/find-lf
 
 # Reset privileges
 chown -R pi:pi /home/pi/
