015707c4430e9e0baba2ef271f05fb30f03a030b
Zack Scholl <zack.scholl@gmail.com>
2017-01-04 14:16:55 -0500

    Implementing continual tshark

--- node/scan.py
+++ node/scan.py
@@ -10,50 +10,51 @@ import socket
 import time
 import subprocess
 import os
+import glob
 import argparse
 import logging
 import statistics
+import atexit
 logger = logging.getLogger('scan.py')
 
 import requests
 
-# try:
-#     import RPi.GPIO as GPIO
-
-#     GPIO_PIN = 3
-#     GPIO.setwarnings(False)
-#     GPIO.setmode(GPIO.BOARD)
-#     GPIO.setup(GPIO_PIN, GPIO.IN)
-# except:
-#     print("GPIO not available")
 
 def restart_wifi():
     os.system("/sbin/ifdown --force wlan0")
     os.system("/sbin/ifup --force wlan0")
     os.system("iwconfig wlan0 mode managed")
     while True:
-        ping_response = subprocess.Popen(["/bin/ping", "-c1", "-w100", "lf.internalpositioning.com"], stdout=subprocess.PIPE).stdout.read()
+        ping_response = subprocess.Popen(
+            ["/bin/ping", "-c1", "-w100", "lf.internalpositioning.com"], stdout=subprocess.PIPE).stdout.read()
         if '64 bytes' in ping_response.decode('utf-8'):
             break
         time.sleep(1)
 
+
 def num_wifi_cards():
     cmd = 'iwconfig'
-    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
     output = p.stdout.read().decode('utf-8')
     return output.count("wlan")
 
 
-def process_scan(output,args):
+def process_scan(time_window):
+    output = ""
+    for filename in glob.glob("/tmp/tshark-temp*"):
+        cmd = subprocess.Popen(("tshark -r "+filename+" -T fields -e frame.time_epoch -e wlan.sa -e wlan.bssid -e radiotap.dbm_antsignal").split(
+        ), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output += cmd.stdout.read().decode('utf-8')
+
+    timestamp_threshold = float(time.time()) - float(time_window)
     fingerprints = {}
     for line in output.splitlines():
         try:
             timestamp, mac, mac2, power_levels = line.split("\t")
 
-            if mac == mac2:
+            if mac == mac2 or float(timestamp) < timestamp_threshold or len(mac) == 0:
                 continue
-
-            timestamp = float(timestamp)
 
             rssi = power_levels.split(',')[0]
             if len(rssi) == 0:
@@ -65,13 +66,15 @@ def process_scan(output,args):
         except:
             pass
 
+    # Compute medians
     fingerprints2 = []
     for mac in fingerprints:
         if len(fingerprints[mac]) == 0:
             continue
         print(mac)
         print(fingerprints[mac])
-        fingerprints2.append({"mac": mac, "rssi": int(statistics.median(fingerprints[mac]))})
+        fingerprints2.append(
+            {"mac": mac, "rssi": int(statistics.median(fingerprints[mac]))})
 
     logger.debug("Processed %d lines, found %d fingerprints" %
                  (len(output.splitlines()), len(fingerprints2)))
@@ -93,42 +96,35 @@ def run_command(command):
     return iter(p.stdout.readline, b'')
 
 
-def run_scan(timeOfScan,wlan):
-    logger.debug("Running scan")
-    # data = []
-    # c = "iw wlan0 info"
-    # logger.debug(c)
-    # for line in run_command(c):
-    #     data.append(line.decode('utf-8'))
-
-    data = []
-    c = "sudo ifconfig %s up" % (wlan)
-    logger.debug(c)
-    run_command(c)
-    
-    data = []
-    c = "/usr/bin/timeout %ds /usr/bin/tshark -I -i %s -T fields -e frame.time_epoch -e wlan.sa -e wlan.bssid -e radiotap.dbm_antsignal" % (timeOfScan,wlan)
-    logger.debug("Running command: '%s'" % c)
-    for line in run_command(c):
-        data.append(line.decode('utf-8'))
-    return "".join(data)
+def tshark_is_running():
+    ps_output = subprocess.Popen(
+        "ps aux".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ps_stdout = ps_output.stdout.read().decode('utf-8')
+    isRunning = 'tshark' in ps_stdout and '[tshark]' not in ps_stdout
+    logger.debug("tshark is running: " + str(isRunning))
+    return isRunning
 
 
-# def check_GPIO(scanningStarted):
-#     try:
-#         val = GPIO.input(GPIO_PIN)
-#         logger.debug("GPIO pin is %d" % int(val))
-#         if scanningStarted and val == 12391023:
-#             os.system('shutdown -r now')
-#         if not scanningStarted and val == 12391023:
-#             sys.exit(1)  # Don't scan yet
-#     except:
-#         pass
+def start_scan(wlan):
+    if not tshark_is_running():
+        # Remove previous files
+        for filename in glob.glob("/tmp/tshark-temp*"):
+            os.remove(filename)
+        subprocess.Popen(("/usr/bin/tshark -I -i " + wlan + " -b files:2 -b filesize:10000 -w /tmp/tshark-temp").split(),
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if tshark_is_running():
+            logger.info("Starting scan")
+
+
+def stop_scan():
+    if tshark_is_running():
+        os.system("pkill -9 tshark")
+        if not tshark_is_running():
+            logger.info("Stopped scan")
 
 
 def main():
     # Check if SUDO
-    # from
     # http://serverfault.com/questions/16767/check-admin-rights-inside-python-script
     if os.getuid() != 0:
         print("you must run sudo!")
@@ -167,10 +163,12 @@ def main():
         help="send payload to this server")
     parser.add_argument("-n", "--nodebug", action="store_true")
     args = parser.parse_args()
+
     # Check arguments for group
     if args.group == "":
         print("Must specify group with -g")
         sys.exit(-1)
+
     # Check arguments for logging
     loggingLevel = logging.DEBUG
     if args.nodebug:
@@ -187,46 +185,42 @@ def main():
     logger.addHandler(fh)
     logger.addHandler(ch)
 
-    # data = []
-    # c = "/usr/bin/timeout 10s /usr/bin/tshark -I -i %s -T fields -e frame.time -e wlan.sa -e wlan.bssid -e radiotap.dbm_antsignal -e wlan.da_resolved" % (wlan)
-    # logger.debug(c)
-    # for line in run_command(c):
-    #     data.append(line.decode('utf-8'))
-    # output1 = "".join(data)
-    # if "doesn't support monitor mode" in output1:
-    #     wlan = "wlan1"
-
-
     # Startup scanning
     print("Using server " + args.server)
     logger.debug("Using server " + args.server)
     print("Using group " + args.group)
     logger.debug("Using group " + args.group)
 
-    time_to_scan = int(args.time)
-    if args.single_wifi:
-        time_to_scan += 5
-
     while True:
         try:
-            scan = run_scan(time_to_scan, args.interface)
             if args.single_wifi:
+                logger.debug("Stopping scan...")
+                stop_scan()
                 logger.debug("Stopping monitor mode...")
                 restart_wifi()
-                logger.debug("...restarted WiFi in managed mode")
-            payload = process_scan(scan, args)
+                logger.debug("Restarting WiFi in managed mode...")
+            start_scan(args.interface)
+            payload = process_scan(args.time)
             payload['group'] = args.group
             if len(payload['signals']) > 0:
                 r = requests.post(
                     args.server +
                     "/reversefingerprint",
                     json=payload)
-                logger.debug(payload)
+                logger.debug(
+                    "Sent to server with status code: " + str(r.status_code))
+            time.sleep(args.time)  # Wait before getting next window
         except:
             e = sys.exc_info()[0]
             logger.error(e)
             logger.debug("Sleeping for 30 seconds")
             time.sleep(30)
 
+
+def exit_handler():
+    print("Exiting...stopping scan..")
+    os.system("pkill -9 tshark")
+
 if __name__ == "__main__":
+    atexit.register(exit_handler)
     main()
